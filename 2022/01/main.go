05b45ee67147a0022c1015e56d34f53fc2524857
Felix Windt <github@felixwindt.com>
2022-12-01 06:57:06 -0500

    2022 day 1

--- 2022/01/main.go
+++ 2022/01/main.go
@@ -4,26 +4,72 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
 )
 
-func readInput(path string) []string {
+// parse input into a list of lists of ints, each list representing the items for an elf
+func ReadInput(path string) [][]int {
 	f, err := os.Open(path)
 	if err != nil {
 		panic(fmt.Sprintf("unable to open %s for reading", path))
 	}
 	defer f.Close()
 
-	var lines []string
+	// list of lists
+	var elves [][]int
+	// the current elf
+	elf := make([]int, 0)
+
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
+		// read next token, default tokenizer returns lines
 		line := scanner.Text()
-		lines = append(lines, line)
+		// if the line is empty, the current elf is done
+		if len(line) == 0 {
+			// add it to the list of elves
+			elves = append(elves, elf)
+			// reset current elf
+			elf = make([]int, 0)
+		} else {
+			// parse the current line into an int64 and add it to the current elf
+			i, err := strconv.ParseInt(line, 10, 64)
+			if err != nil {
+				panic(fmt.Sprintf("unable to convert '%s' into an integer", line))
+			}
+			elf = append(elf, int(i))
+		}
 	}
 
-	return lines
+	return elves
 }
 
 func main() {
-	input := readInput(os.Args[1])
-	fmt.Printf("%v", input)
+	elves := ReadInput(os.Args[1])
+
+	// does go have a build in sum function? like hell it does
+	// we're real programmers and know how to loop
+	var sums []int
+	for _, elf := range elves {
+		sum := 0
+		for _, item := range elf {
+			sum += item
+		}
+		sums = append(sums, sum)
+	}
+
+	// does it have a max function? ha, you're kidding, right?
+	max := 0
+	for _, sum := range sums {
+		if sum > max {
+			max = sum
+		}
+	}
+
+	fmt.Printf("Solution 1: %d\n", max)
+
+	// alright, then, for part 2 we'll sort properly
+	sort.Ints(sums)
+	topThree := sums[len(sums)-3 : len(sums)]
+	fmt.Printf("Solution 2: %d\n", topThree[0]+topThree[1]+topThree[2])
 }
