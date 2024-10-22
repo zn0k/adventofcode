8ff0059e98ad60c76a02d271226ae445db5c56bb
Felix Windt <felix.windt@dartmouth.edu>
2024-10-22 15:28:01 -0400

    random commit before swapping out laptop

--- 2023/18/src.py
+++ 2023/18/src.py
@@ -48,42 +48,35 @@ def area(nodes):
 def hex2rgb(hex):
   return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
 
-def saveImg(tiles):
-  xs = [v[0] for v in tiles.keys()]
-  ys = [v[1] for v in tiles.keys()]
-  x_offset = 0 - min(xs)
-  y_offset = 0 - min(ys)
-  max_x = x_offset + max(xs) + 1
-  max_y = y_offset + max(ys) + 1
-  img = Image.new("RGB", (max_x, max_y))
-  for coordinate, color in tiles.items():
-    x, y = coordinate[0] + x_offset, coordinate[1] + y_offset
-    img.putpixel((x, y), hex2rgb(color))
-  img.save("out.png")
-
 def neighbors(x, y):
   return [(x + dx, y + dy)for dx, dy in directions.values()]
 
 # we had some clever algorithms to determine the first tile to flood
 # they didn't work, so we looked at the picture instead. ok then.
-#first = (-1, -1)
+first = (-1, -1)
 # now floodfill from that space
-#q = deque([first])
-#while len(q):
-#  x, y = q.popleft()
-#  tiles[(x, y)] = "000000"
-#  next = [x for x in neighbors(x, y) if x not in tiles and x not in q]
-#  q.extend(next)
+q = deque([first])
+while len(q):
+  x, y = q.popleft()
+  tiles[(x, y)] = "000000"
+  next = [x for x in neighbors(x, y) if x not in tiles and x not in q]
+  q.extend(next)
 
-#saveImg(tiles)
+print(f"Solution 1: {len(tiles)}")
 
-#print(f"Solution 1: {len(tiles)}")
-print(area(nodes))
+# b = integer points on boundary
+# i = integer points inside polygon
+# A = i + (b/2) - 1
 
-#(1, 2)
-#(3, 4)
-#(5, 6)
-#
-#as = 1 * 4 + 3 * 6
-#bs = 2 * 3 + 4 * 5
-#area = (as + bs) / 2
+def pick(nodes):
+  b = 0
+  current = nodes[0]
+  for node in nodes:
+    if current[0] == node[0]:
+      b += abs(current[1] - node[1])
+    else:
+      b += abs(current[0] - node[0])
+    current = node
+  return b
+
+print(pick(nodes))
