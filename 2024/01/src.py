dc476de7cba69ad32c12ba99b02a527e60b0dda7
Felix Windt <felix.windt@dartmouth.edu>
2024-12-01 06:31:57 -0500

    2024/01

---
+++ 2024/01/src.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+import numpy as np
+from collections import Counter
+
+with open(sys.argv[1], "r") as f: 
+  lines = [l.strip() for l in f.readlines()]
+data = np.array([[int(x) for x in l.split()] for l in lines])
+
+left, right = sorted(data[:, 0]), sorted(data[:, 1])
+diffs = [abs(x[0] - x[1]) for x in zip(left, right)]
+#diffs = map(lambda x: abs(x[0] - x[1]), zip(left, right))
+print(f"Solution 1: {sum(diffs)}")
+
+c = Counter(right)
+scores = [x * (c[x] if x in c else 0) for x in left]
+print(f"Solution 2: {sum(scores)}")
